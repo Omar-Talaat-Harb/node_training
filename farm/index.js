d41e1094fcc58f9fa30f2f5af7b1010bbc28e2e7
Omar-Talaat-Harb <omartalaatharb@gmail.com>
2024-10-01 18:12:24 +0300

    my first project update module

--- farm/index.js
+++ farm/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate =require('./modules/replaceTemplate');
+
 //blocking , synchronous way
 // const textIn =fs.readFileSync('./txt/input.txt','utf-8');
 // console.log(textIn);
@@ -19,7 +21,7 @@ fs.readFile('./txt/start.txt', 'utf-8',(err,data1)=>{
             console.log(data3);
 
             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,err=>{
-                console.log('your file has been written 😋');
+                console.log('your file has been written ');
             })
         });
     });
@@ -27,20 +29,6 @@ fs.readFile('./txt/start.txt', 'utf-8',(err,data1)=>{
 console.log('will read file');
 */
 //server
-const replaceTemplate = (temp,product)=>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%DESCRIPT%}/g, product.description);
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-
-};
-
 const tempOverview =fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard =fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct =fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
@@ -73,7 +61,7 @@ const server = http.createServer((req,res)=>{
     
     //api    
     }else if(pathname === '/api'){
-        res.writeHead(200,{'Content-tye':'application/json'});
+        res.writeHead(200,{'Content-type':'application/json'});
         res.end(data);
 
     //not found    
